5372eb915fd93c8edbe5d73fc4f0de01d987571d
cutplane1 <snakesnake222@mail.ru>
2025-04-07 21:23:19 +0500

    yay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import win32gui, win32con, win32api
-import time
+import time, sys
 
 class PhysicsWindow:
     def __init__(self, hwnd):
@@ -8,9 +8,16 @@ class PhysicsWindow:
         self.dragging = False
         self.drag_offset = (0, 0)
         self.last_cursor_pos = (0, 0)
-        rect = win32gui.GetWindowRect(hwnd)
+        self.update_position()
+
+    def update_position(self):
+        rect = win32gui.GetWindowRect(self.hwnd)
         self.x, self.y = rect[0], rect[1]
         self.width, self.height = rect[2]-self.x, rect[3]-self.y
+
+def is_window_maximized(hwnd):
+    placement = win32gui.GetWindowPlacement(hwnd)
+    return placement[1] == win32con.SW_SHOWMAXIMIZED
 
 class WindowPhysicsEngine:
     def __init__(self):
@@ -18,6 +25,7 @@ class WindowPhysicsEngine:
         self.bounce = 0.65
         self.friction = 0.96
         self.windows = []
+        self.temp_windows = []
         self.screen_width = win32api.GetSystemMetrics(0)
         self.screen_height = win32api.GetSystemMetrics(1) - 30
 
@@ -26,21 +34,24 @@ class WindowPhysicsEngine:
             self.windows.append(PhysicsWindow(hwnd))
 
     def handle_input(self):
-        self.windows = [w for w in self.windows if win32gui.IsWindow(w.hwnd)]
+        self.temp_windows = [w for w in self.windows if win32gui.IsWindow(w.hwnd) and not is_window_maximized(w.hwnd)]
         cursor_pos = win32api.GetCursorPos()
         left_down = win32api.GetAsyncKeyState(win32con.VK_LBUTTON) < 0
 
-        if left_down and not any(w.dragging for w in self.windows):
-            for w in reversed(self.windows):
-                rect = win32gui.GetWindowRect(w.hwnd)
-                w.x, w.y = rect[0], rect[1]
-                if w.x <= cursor_pos[0] <= w.x + w.width and w.y <= cursor_pos[1] <= w.y + w.height:
-                    w.dragging = True
-                    w.drag_offset = (cursor_pos[0] - w.x, cursor_pos[1] - w.y)
-                    w.last_cursor_pos = cursor_pos
+        if left_down and not any(w.dragging for w in self.temp_windows):
+            hwnd = win32gui.WindowFromPoint(cursor_pos)
+            top_hwnd = win32gui.GetAncestor(hwnd, win32con.GA_ROOT)
+            
+            for w in self.temp_windows:
+                if w.hwnd == top_hwnd:
+                    w.update_position()
+                    if w.x <= cursor_pos[0] <= w.x + w.width and w.y <= cursor_pos[1] <= w.y + w.height:
+                        w.dragging = True
+                        w.drag_offset = (cursor_pos[0] - w.x, cursor_pos[1] - w.y)
+                        w.last_cursor_pos = cursor_pos
                     break
         elif left_down:
-            for w in self.windows:
+            for w in self.temp_windows:
                 if w.dragging:
                     dx = cursor_pos[0] - w.last_cursor_pos[0]
                     dy = cursor_pos[1] - w.last_cursor_pos[1]
@@ -50,11 +61,11 @@ class WindowPhysicsEngine:
                     self._move_window(w)
                     w.last_cursor_pos = cursor_pos
         else:
-            for w in self.windows:
+            for w in self.temp_windows:
                 w.dragging = False
 
     def update_physics(self):
-        for w in self.windows:
+        for w in self.temp_windows:
             if w.dragging:
                 continue
 
@@ -80,16 +91,32 @@ class WindowPhysicsEngine:
         except Exception as e:
             print(f"Move error: {e}")
 
+
+def list_all_windows():
+    def callback(hwnd, windows):
+        text = win32gui.GetWindowText(hwnd)
+        # :(((((((((((((((((((((((
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) and text != "Параметры" and text != "Медиаплеер" and text != "Microsoft Text Input Application" and text != "Program Manager":
+            windows.append((hwnd, text))
+        return True
+
+    windows = []
+    win32gui.EnumWindows(callback, windows)
+    return windows
+
 def main():
     engine = WindowPhysicsEngine()
-    engine.add_window(win32gui.FindWindow(None, "Безымянный – Блокнот"))
-    engine.add_window(win32gui.FindWindow(None, "qBittorrent v4.6.4"))
-    engine.add_window(win32gui.FindWindow(None, "Командная строка"))
 
-    while True:
-        engine.handle_input()
-        engine.update_physics()
-        time.sleep(1/60)
+    for hwnd, _ in list_all_windows():
+        engine.add_window(hwnd)
+
+    try:
+        while True:
+            engine.handle_input()
+            engine.update_physics()
+            time.sleep(1/60)
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
